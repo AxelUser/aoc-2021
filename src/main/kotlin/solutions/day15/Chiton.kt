9e4c646dc88011b5c793ad13f0ebdc4f0ca06f7b
Alexey Maltsev <axel.user.perm@gmail.com>
2021-12-15 11:42:13 +0300

    Solved day 15

---
+++ src/main/kotlin/solutions/day15/Chiton.kt
@@ -0,0 +1,77 @@
+package solutions.day15
+
+import solutions.BaseSolution
+import java.util.*
+
+class Chiton: BaseSolution() {
+    override fun part1(input: String): Any {
+        return input.solve()
+    }
+
+    override fun part2(input: String): Any {
+        return input.solve(5)
+    }
+
+    private fun String.solve(scale: Int = 1): Int {
+        return parseMatrix().let { matrix ->
+            dijkstra(Pair(0, 0), matrix.bottomRight(scale)){ point ->
+                matrix.getAdjacent4(point, scale).map { adj -> State(adj, matrix.getRisk(adj)) }
+            }
+        }
+    }
+
+    private data class State(val point: Pair<Int, Int>, val risk: Int)
+
+    private fun dijkstra(start: Pair<Int, Int>, destination: Pair<Int, Int>, getAdjacent: (Pair<Int, Int>) -> Sequence<State>): Int {
+        val risks = mutableMapOf(start to 0)
+        val pq = PriorityQueue<State>(compareBy { it.risk })
+        pq.add(State(start, 0))
+
+        while (pq.isNotEmpty()) {
+            val closest = pq.remove()
+            for (adj in getAdjacent(closest.point)) {
+                val newRisk = adj.risk + closest.risk
+                if (risks.getOrDefault(adj.point, Int.MAX_VALUE) > newRisk) {
+                    risks[adj.point] = newRisk
+                    pq.add(State(adj.point, newRisk))
+                }
+            }
+        }
+
+        return risks.getValue(destination)
+    }
+
+    private fun Array<IntArray>.getAdjacent4(point: Pair<Int, Int>, scale: Int = 1): Sequence<Pair<Int, Int>> {
+        val (i, j) = point
+        val length = this@getAdjacent4.count() * scale
+        val width = this@getAdjacent4[0].count() * scale
+        return sequence {
+            if (i > 0) yield(Pair(i - 1, j)) // top
+            if (i < (length - 1)) yield(Pair(i + 1, j)) // bottom
+            if (j > 0) yield(Pair(i, j - 1)) // left
+            if (j < (width - 1)) yield(Pair(i, j + 1)) // right
+        }
+    }
+
+    private fun Array<IntArray>.getRisk(point: Pair<Int, Int>): Int {
+        val (y, x) = point
+        val length = this@getRisk.count()
+        val width = this@getRisk[0].count()
+
+        val increase = y / length + x / width
+        return (((this[y % length][x % width] - 1) + increase) % 9) + 1
+    }
+
+    private fun Array<IntArray>.bottomRight(scale: Int = 1): Pair<Int, Int> {
+        return Pair(count() * scale - 1, first().count() * scale - 1)
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun String.parseMatrix(): Array<IntArray> {
+        return this.lines().map { l -> l.map { c -> c.digitToInt() }.toIntArray() }.toTypedArray()
+    }
+}
+
+fun main() {
+    Chiton().runAll()
+}
